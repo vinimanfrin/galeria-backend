809d9890991f5b7c86aa21ae64a34b66a4829887
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-23 16:45:23 -0300

    criando um mapper para image , e retornando a uri da image criada

--- src/main/java/vinimanfrin/imageapi/application/images/ImageController.java
+++ src/main/java/vinimanfrin/imageapi/application/images/ImageController.java
@@ -2,17 +2,16 @@ package vinimanfrin.imageapi.application.images;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import vinimanfrin.imageapi.domain.entity.Image;
-import vinimanfrin.imageapi.domain.enums.ImageExtension;
-
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,16 +31,19 @@ public class ImageController {
 
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",",tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueoOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = ImageMapper.mapToImage(file,name,tags);
+        Image savedImage = imageService.save(image);
+        URI imageUri = buildImageURL(savedImage);
 
-        imageService.save(image);
+        return ResponseEntity.created(imageUri).build();
+    }
 
-        return ResponseEntity.ok().build();
+    private URI buildImageURL(Image image){
+        String imagePath = "/"+image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build()
+                .toUri();
     }
 }
---
+++ src/main/java/vinimanfrin/imageapi/application/images/ImageMapper.java
@@ -0,0 +1,26 @@
+package vinimanfrin.imageapi.application.images;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import vinimanfrin.imageapi.domain.entity.Image;
+import vinimanfrin.imageapi.domain.enums.ImageExtension;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ImageMapper {
+
+    public static Image mapToImage(MultipartFile file, String name, List<String> tags ) throws IOException {
+        Image image = Image.builder()
+                .name(name)
+                .tags(String.join(",",tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueoOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+
+        return image;
+    }
+}
